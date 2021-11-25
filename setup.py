ff2fbf2d1b98ccbe00e18733e3cc936c21f605a9
Philippe <nphilou@gmail.com>
2021-11-25 11:44:25 +0100

    Remove base packages

--- setup.py
+++ setup.py
@@ -15,10 +15,8 @@ setup(
           'pandas',
           'matplotlib',
           'scikit-learn',
-          'pickle',
           'PyWavelets',
           'scipy',
-          'pprint',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the
